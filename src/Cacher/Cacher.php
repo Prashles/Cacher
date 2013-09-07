1b586e85820e798dcfa5d2a413734c629b1a7473
Prash <prash.somaiya@gmail.com>
2013-09-08 00:25:55 +0100

    Weird indenting error

--- src/Cacher/Cacher.php
+++ src/Cacher/Cacher.php
@@ -15,28 +15,28 @@ class Cacher {
 	 * @return mixed
 	 */
 	public static function __callStatic($function, $args)
-    {
-    	// If no config file is set, inject default
-    	if (self::$config === null) {
-    		self::$config = include_once __DIR__ . '/config.php';
-    	}
+	{
+		// If no config file is set, inject default
+		if (self::$config === null) {
+			self::$config = include_once __DIR__ . '/config.php';
+		}
 
-    	$method = 'make' . ucfirst(self::$config['general']['method']) . 'Method';
+		$method = 'make' . ucfirst(self::$config['general']['method']) . 'Method';
 
-    	$methodInstance = self::$method();
+		$methodInstance = self::$method();
 
-    	$instance = new CacherService(self::$config, $methodInstance);
+		$instance = new CacherService(self::$config, $methodInstance);
 
-        return call_user_func_array([$instance, $function], $args);
-    }
+		return call_user_func_array([$instance, $function], $args);
+	}
 
-    /**
-     * Instantiate FileCache class
-     * 
-     * @return FileCache
-     */
-    public static function makeFileMethod()
-    {
-    	return new FileCache(self::$config['file']['path'], self::$config['file']['prefix']);
-    }
+	/**
+	 * Instantiate FileCache class
+	 * 
+	 * @return FileCache
+	 */
+	public static function makeFileMethod()
+	{
+		return new FileCache(self::$config['file']['path'], self::$config['file']['prefix']);
+	}
 }
